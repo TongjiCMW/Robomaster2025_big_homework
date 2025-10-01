7e770c6fef676541040c3f3a1e7b9dcafa3810c3
TongjiCMW <2353282@tongji.edu.cn>
2025-10-01 21:49:32 +0800

    init

---
+++ applications/servo_task.cpp
@@ -0,0 +1,18 @@
+#include "cmsis_os.h"
+#include "io/servo/servo.hpp"
+
+// C板
+sp::Servo servo(&htim1, TIM_CHANNEL_1, 168e6f, 180.0f);  // 开发板最上面的PWM端口, 180度舵机
+
+// 达妙
+// sp::Servo servo(&htim1, TIM_CHANNEL_3, 240e6f, 180.0f); // 开发板最上面的PWM端口, 180度舵机
+
+extern "C" void servo_task()
+{
+  servo.start();
+
+  for (float angle = 0; angle <= 181; angle += 45) {
+    servo.set(angle);
+    osDelay(1000);
+  }
+}
