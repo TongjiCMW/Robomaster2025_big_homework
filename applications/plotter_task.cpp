a92a051f1b54fc93f075037b05fcbe671b56ba51
TongjiCMW <2353282@tongji.edu.cn>
2025-10-02 19:25:00 +0800

    plotter发送遥控器和电机转速

--- applications/plotter_task.cpp
+++ applications/plotter_task.cpp
@@ -3,14 +3,14 @@
 #include "io/dbus/dbus.hpp"
 #include "io/plotter/plotter.hpp"
 #include "motor/rm_motor/rm_motor.hpp"
-
+extern sp::RM_Motor motor6020_1;
 extern sp::DBus remote_controller;
 sp::Plotter plotter(&huart1);
 
 extern "C" void plotter_task()
 {
   while (true) {
-    plotter.plot(remote_controller.ch_lh);
+    plotter.plot(remote_controller.ch_lh, motor6020_1.speed);
     osDelay(10);  // 100Hz
   }
 }
