e22f0bcdc6ac17d4f1b7270f455514f15ed9316c
TongjiCMW <2353282@tongji.edu.cn>
2025-10-03 00:04:51 +0800

    test:测试遥控器左摇杆范围

--- applications/plotter_task.cpp
+++ applications/plotter_task.cpp
@@ -14,7 +14,8 @@ sp::Plotter plotter(&huart1);
 extern "C" void plotter_task()
 {
   while (true) {
-    plotter.plot(motor3508_1.speed, motor3508_2.speed, motor3508_3.speed, motor3508_4.speed);
+    //plotter.plot(motor3508_1.speed, motor3508_2.speed, motor3508_3.speed, motor3508_4.speed);
+    plotter.plot(remote_controller.ch_lh, remote_controller.ch_lv);
     osDelay(10);  // 100Hz
   }
 }
